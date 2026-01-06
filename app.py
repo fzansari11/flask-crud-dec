52ec60a66fe4bfbb42e67306e5410db3fbadc8f1
Zee <fzansari11@gmail.com>
2026-01-05 20:05:55 -0500

    S3 support

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-
+import boto3
 import os
+import logging 
 
 app = Flask(__name__)
+
+logging.basicConfig(level=logging.INFO)
 
 # Configure the database URI. Using SQLite file-based DB here
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -18,6 +21,17 @@ print ("The final database path is ", final_db_path)
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///' + final_db_path
 app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://admin:Cloudberry123@database-1.c09gg8s4sv82.us-east-1.rds.amazonaws.com/tasks'
 db = SQLAlchemy(app)
+
+BUCKET_NAME = 'flask-dec-todo-s3'
+
+def upload_file_to_s3(file_path, file_name):
+    s3 = boto3.client('s3')
+    try:
+        s3.upload_file(file_path, BUCKET_NAME, file_name)
+        logging.info(f"File {file_name} uploaded to S3 bucket {BUCKET_NAME}")
+    except Exception as e:
+        logging.error(f"Error uploading file to S3: {e}")
+        return None
 
 class Task(db.Model):
     __tablename__ = 'tasks'
@@ -39,6 +53,14 @@ def home():
 def add_task():
     task = request.form.get('task')
     new_task = Task(title=task)
+    file = request.files['file']
+    if file:
+        logging.info(f"Received file: {file.filename}")
+        file_path = os.path.join(basedir, file.filename)
+        file.save(file_path)
+        upload_file_to_s3(file_path, file.filename)
+        os.remove(file_path)
+
     db.session.add(new_task)
     db.session.commit()
     return redirect('/')
